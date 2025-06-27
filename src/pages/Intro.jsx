5ecd5129c44765d959346b71d7b9a2436b138aa5
AndressToscanom30 <andress.toscano@gmail.com>
2025-06-27 11:11:22 -0500

    Intro modified

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,31 +1,14 @@
-import { useEffect, useState } from "react";
-import '../Intro.css';
+import './Intro.css';
 
-function Intro() {
-  const [hideIntro, setHideIntro] = useState( () => {
-    return localStorage.getItem("introSeen") === "true";
-  });
-
-  useEffect(() => {
-    if (!hideIntro) {
-      const timeout = setTimeout(() => {
-        setHideIntro(true);
-        localStorage.setItem("introSeen", "true");
-      }, 5000);
-      return () => clearTimeout(timeout);
-    }
-  }, [hideIntro]);
-
-  if (hideIntro) return null;
-
+const Intro = () => {
   return (
     <div className="intro-container">
       <div className="video-mask">
         <video src="/videoFondo.mp4" autoPlay muted loop className="video-in-text" />
-        <h1 className="masked-text">CÚCUTA</h1>
+        <h1 className="masked-text">CUCUTA</h1>
       </div>
     </div>
   );
-}
+};
 
 export default Intro;
