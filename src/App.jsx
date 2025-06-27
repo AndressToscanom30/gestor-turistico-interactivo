67cfc11a95d2a4be6b99fd9b87900f5dd85a97a3
AndressToscanom30 <andress.toscano@gmail.com>
2025-06-27 11:08:47 -0500

    Prueba 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Inicio from './pages/inicio.jsx';
 import Navbar from './components/Navbar.jsx';
@@ -11,25 +12,40 @@ import AventuraVerde from './pages/AventuraVerde.jsx';
 import Intro from './pages/Intro.jsx';
 import Footer from './components/Footer.jsx';
 
-
 function App() {
+  const [showIntro, setShowIntro] = useState(() => {
+    return localStorage.getItem('introSeen') !== 'true';
+  });
+
+  useEffect(() => {
+    if (showIntro) {
+      const timer = setTimeout(() => {
+        setShowIntro(false);
+        localStorage.setItem('introSeen', 'true');
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [showIntro]);
 
   return (
-    <>
     <BrowserRouter basename='/'>
-      <Intro />
-      <Navbar />
-      <Routes>
-        <Route path='/inicio' element={<Inicio />}></Route>
-        <Route path='/cultura-historia' element={<CulturaHistoria />}></Route>
-        <Route path='/gastro-comercio' element={<GastroComercio />}></Route>
-        <Route path='/turismo-religioso' element={<TurismoReligioso />}></Route>
-        <Route path='/aventura-verde' element={<AventuraVerde />}></Route>
-      </Routes>
-      <Footer />
+      {showIntro && <Intro />}
+      {!showIntro && (
+        <>
+          <Navbar />
+          <Routes>
+            <Route path='/inicio' element={<Inicio />} />
+            <Route path='/cultura-historia' element={<CulturaHistoria />} />
+            <Route path='/gastro-comercio' element={<GastroComercio />} />
+            <Route path='/turismo-religioso' element={<TurismoReligioso />} />
+            <Route path='/aventura-verde' element={<AventuraVerde />} />
+          </Routes>
+          <Footer />
+        </>
+      )}
     </BrowserRouter>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
