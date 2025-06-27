90663dfcaab618a26459190043fd34358d736dfa
AndressToscanom30 <andress.toscano@gmail.com>
2025-06-27 09:30:22 -0500

    Correcciones 0.1

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Inicio from './pages/inicio.jsx';
-import Navbar from './components/navBar.jsx';
+import Navbar from './components/NavBar.jsx';
 import CulturaHistoria from './pages/CulturaHistoria.jsx';
-import GastroComercio from './pages/gastroComercio.jsx';
-import TurismoReligioso from './pages/turismoReligioso.jsx';
+import GastroComercio from './pages/GastroComercio.jsx';
+import TurismoReligioso from './pages/TurismoReligioso.jsx';
 import AventuraVerde from './pages/AventuraVerde.jsx';
 import Intro from './pages/Intro.jsx';
 import Footer from './components/Footer.jsx';
